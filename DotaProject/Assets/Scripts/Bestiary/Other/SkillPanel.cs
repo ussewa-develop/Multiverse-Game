489d322c6cdca568e21619942dc06661342489ca
ussewa-develop <ussewa404@gmail.com>
2023-09-02 01:52:29 +0300

    Test

--- DotaProject/Assets/Scripts/Bestiary/Other/SkillPanel.cs
+++ DotaProject/Assets/Scripts/Bestiary/Other/SkillPanel.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class SkillPanel : MonoBehaviour
 {
+    [Header("\t\t\tSkill variables")]
     public TextMeshProUGUI skillNameText;
     public TextMeshProUGUI skillTypeText;
     public TextMeshProUGUI skillDescText;
@@ -11,13 +12,34 @@ public class SkillPanel : MonoBehaviour
     public TextMeshProUGUI skillCooldownText;
     public TextMeshProUGUI skillManaText;
     public TextMeshProUGUI skillActionPointText;
+    public TextMeshProUGUI skillDamageTypeText;
+    [Space]
+    [Header("\t\t\tOther")]
     public GameObject background;
     public GameObject toolTip;
     public TextMeshProUGUI textTip;
-    public TextMeshProUGUI damageTypeText;
-    public TextMeshProUGUI cooldownText;
-    public TextMeshProUGUI manaText;
-    public TextMeshProUGUI actionPointText;
+    [SerializeField] GameObject downStats;
+
+    private void Start()
+    {
+        EventController.SwitchMenu += Destroy;
+        Vector3 downStatsPos = downStats.transform.localPosition;
+        downStatsPos.y = background.transform.localPosition.y*2;
+        downStats.transform.localPosition = downStatsPos;
+
+        float delta = skillDescText.preferredHeight/43;
+        background.transform.localScale += new Vector3 (0,delta*40,0);
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        EventController.SwitchMenu -= Destroy;
+    }
 
 
 
@@ -27,23 +49,22 @@ public class SkillPanel : MonoBehaviour
         skillDescText.text = skill.skillDesc;
         skillIcon.sprite = skill.skillIcon;
         skillTypeText.text = Localizator.Localize("Ability") + Localizator.Localize(skill.skillType.ToString());
-        cooldownText.text = skill.cooldown + " " + Localizator.Localize("Turn");
-        manaText.text = Localizator.Localize("Mana") + ": " + skill.manaCost;
-        actionPointText.text = Localizator.Localize("AP") + ": " + skill.actionPointCost;
+        skillCooldownText.text = skill.cooldown + " " + Localizator.Localize("Turn");
+        skillManaText.text = Localizator.Localize("Mana") + ": " + skill.manaCost;
+        skillActionPointText.text = Localizator.Localize("AP") + ": " + skill.actionPointCost;
         if (skill.IsHasSomeSkills)
         {
             toolTip.SetActive(true);
         }
         if (skill.IsDamaging)
         {
-            damageTypeText.gameObject.SetActive(true);
+            skillDamageTypeText.gameObject.SetActive(true);
 
-            damageTypeText.text = Localizator.Localize("DamageType") +
+            skillDamageTypeText.text = Localizator.Localize("DamageType") +
                 IconLoader.LoadSmile(skill.damageType) + Localizator.Localize(skill.damageType.ToString());
         }
         transform.position = icon.position - new Vector3(0, 3.5f, 0);
     }
-
 
     #region SetSkillInPanel overrides
     public void SetSkillInPanel(SkillSO skill,Transform icon,Canvas canvas)
@@ -64,18 +85,4 @@ public class SkillPanel : MonoBehaviour
     }
     #endregion
 
-    public void Destroy()
-    {
-        Destroy(gameObject);
-    }
-
-    private void Start()
-    {
-        EventController.SwitchMenu += Destroy;
-    }
-
-    private void OnDestroy()
-    {
-        EventController.SwitchMenu -= Destroy;
-    }
 }
